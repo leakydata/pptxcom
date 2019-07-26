f65e6fb534b48264be1ad71d9e053251abd1c024
Nathan Jones <scholyx@gmail.com>
2019-07-26 13:05:16 -0400

    Update SelectionFunctions.py

--- examples/SelectionFunctions.py
+++ examples/SelectionFunctions.py
@@ -2,7 +2,17 @@ import win32com.client as win32
 
 # Create/Assign an Application object from the currently active PowerPoint Instance
 PPT = win32.GetActiveObject("PowerPoint.Application") 
-#pres = PPT.ActivePresentation #Grab the already open powerpoint file (the active PowerPoint Presentation)
     
-    
-PPT.ActivePresentation.Slides(1)
+# Goto a specific slide number (slide view)
+PPT.ActiveWindow.View.GotoSlide(1)
+
+# Select a shape on a slide by it's number (the slide must be the current active slide)
+PPT.ActivePresentation.Slides(1).Shapes(2).Select(-1)
+
+# Select a shape it's Name (the slide must be the current active slide)
+PPT.ActivePresentation.Slides(1).Shapes('StandardCode_IQNum').Select(-1)
+
+
+# Print out the Type number/constant value, of every Shape on the active slide numbered 1 
+for s in PPT.ActivePresentation.Slides(1).Shapes:
+    print(s.Type)
