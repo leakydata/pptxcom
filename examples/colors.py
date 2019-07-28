ae1648717eebc5aee41e7181969a5dccf1618d94
Nathan Jones <scholyx@gmail.com>
2019-07-28 12:49:06 -0400

    Update colors.py

--- examples/colors.py
+++ examples/colors.py
@@ -1,9 +1,5 @@
-def rgb_to_hex(rgb):
-    '''
-    ws.Cells(1, i).Interior.color uses bgr in hex
-    '''
-    bgr = (rgb[2], rgb[1], rgb[0])
-    strValue = '%02x%02x%02x' % bgr
-    # print(strValue)
-    iValue = int(strValue, 16)
-    return iValue
+def rgb_to_hex(rgb): # color uses bgr in hex
+    bgr = (rgb[2], rgb[1], rgb[0]) # convert rgb into bgr
+    str_value = '%02x%02x%02x' % bgr # convert bgr in hex
+    int_value = int(str_value, 16) # convert hex to integer
+    return int_value
