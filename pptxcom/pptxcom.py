a7ac1f3b9c63e0fb40fb8658d5111c6bacf16b8d
Nathan Jones <scholyx@gmail.com>
2019-08-04 15:08:28 -0400

    Update pptxcom.py

--- pptxcom/pptxcom.py
+++ pptxcom/pptxcom.py
@@ -7,6 +7,7 @@ Created on Sun Aug  4 14:00:36 2019
 
 import win32com.client as win32Client
 import win32com.client.gencache as win32ClientGen
+from win32com.client import constants as constants
 from pywintypes import com_error  # pylint: disable=I0011,E0611
 
 def grab_active(visible=True):
