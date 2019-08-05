7675c6bf49434e69b05d1fd87f239debe5adc02d
Nathan Jones <scholyx@gmail.com>
2019-08-04 22:21:07 -0400

    Update pptxcom.py

--- pptxcom/pptxcom.py
+++ pptxcom/pptxcom.py
@@ -4,53 +4,55 @@ Created on Sun Aug  4 14:00:36 2019
 
 @author: Nathan Jones
 """
+
 import os
+
 import win32com.client as win32Client
 import win32com.client.gencache as win32ClientGen
 
 from pywintypes import com_error  # pylint: disable=I0011,E0611
 
 def active_app(visible=True):
-    """
-    @visible: Set PowerPoint application window to visible
-    
-    Grabs the active PowerPoint application and creates a COM object
-    representing the application.
-    """
-    
-    try:
-        p = win32Client.GetActiveObject("PowerPoint.Application")
-    except com_error:
-        p = win32ClientGen.EnsureDispatch("PowerPoint.Application")
-    
-    # If Visible = False we will get a COM error since an Open Applivation window will always be visible
-    # Visible is set here to avoid vagueness, but it is basically optional in this specific case
-    p.Visible = visible 
+	"""
+	@visible: Set PowerPoint application window to visible
+	
+	Grabs the active PowerPoint application and creates a COM object
+	representing the application.
+	"""
+	
+	try:
+		p = win32Client.GetActiveObject("PowerPoint.Application")
+	except com_error:
+		p = win32ClientGen.EnsureDispatch("PowerPoint.Application")
+	
+	# If Visible = False we will get a COM error since an Open Applivation window will always be visible
+	# Visible is set here to avoid vagueness, but it is basically optional in this specific case
+	p.Visible = visible 
 
-    return p
+	return p
 
 
 def open(filepath, visible=True):
-    """
-    @filepath: raw text file path r'C:\path\to\the\powerpoint_file.pptx'
-    @visible: Set PowerPoint application window to visible
-    
-    Opens a PowerPoint file creates a COM object representing the presentation.
-    """
+	"""
+	@filepath: raw text file path r'C:\path\to\the\powerpoint_file.pptx'
+	@visible: Set PowerPoint application window to visible
+	
+	Opens a PowerPoint file creates a COM object representing the presentation.
+	"""
 
-    try:
-        os.path.isfile(filepath)
-    except ValueError:
-        print("File does not exist.")
-        return False
-    
-    try:
-        p = win32Client.Dispatch("PowerPoint.Application")
-    except com_error:
-        print("File not available.")   
-        return False
-        
-    p.Visible = visible
-    pres = p.Presentations.Open(filepath) 
-    
-    return pres
+	try:
+		os.path.isfile(filepath)
+	except ValueError:
+		print("File does not exist.")
+		return False
+	
+	try:
+		p = win32Client.Dispatch("PowerPoint.Application")
+	except com_error:
+		print("File not available.")   
+		return False
+		
+	p.Visible = visible
+	pres = p.Presentations.Open(filepath) 
+	
+	return pres
