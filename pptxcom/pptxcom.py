08cebb711ddb14399fc7d33ef0eb2f0e208b13c8
Nathan Jones <scholyx@gmail.com>
2019-08-04 21:54:47 -0400

    Update pptxcom.py

--- pptxcom/pptxcom.py
+++ pptxcom/pptxcom.py
@@ -10,7 +10,7 @@ import win32com.client.gencache as win32ClientGen
 
 from pywintypes import com_error  # pylint: disable=I0011,E0611
 
-def grab_active(visible=True):
+def get_active_app(visible=True):
     """
     @visible: Set PowerPoint application window to visible
     
@@ -29,12 +29,34 @@ def grab_active(visible=True):
 
     return p
 
+
+
+def get_active_pres(visible=True):
+    """
+    @visible: Set PowerPoint application window to visible
+    
+    Grabs the active PowerPoint application and creates a COM object
+    representing the active presentation.
+    """
+    
+    try:
+        p = win32Client.GetActiveObject("PowerPoint.Application")
+    except com_error:
+        p = win32ClientGen.EnsureDispatch("PowerPoint.Application")
+    
+    # If Visible = False we will get a COM error since an Open Applivation window will always be visible
+    # Visible is set here to avoid vagueness, but it is basically optional in this specific case
+    p.Visible = visible 
+
+    return p.ActivePresentation
+
+
 def open(filepath, visible=True):
     """
     @filepath: raw text file path r'C:\path\to\the\powerpoint_file.pptx'
     @visible: Set PowerPoint application window to visible
     
-    Opens a PowerPoint file creates a COM object representing the application.
+    Opens a PowerPoint file creates a COM object representing the presentation.
     """
 
     try:
@@ -50,6 +72,6 @@ def open(filepath, visible=True):
         return False
         
     p.Visible = visible
-    pres = p.Presentations.Open(filepath)   
+    pres = p.Presentations.Open(filepath) 
     
     return pres
