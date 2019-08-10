c2728ba3fb01093854a38ef05422486646e68f5e
Nathan Jones <scholyx@gmail.com>
2019-08-10 16:20:27 -0400

    Update pptxcom.py

--- pptxcom/pptxcom.py
+++ pptxcom/pptxcom.py
@@ -68,3 +68,10 @@ def open_pres(filepath, visible=True):
 	pres = p.Presentations.Open(filepath) 
 	
 	return pres
+
+# Convert standard RGB colors to PowerPoint color integer values
+def rgb_to_hex(rgb): # color uses bgr in hex
+    bgr = (rgb[2], rgb[1], rgb[0]) # convert rgb into bgr
+    str_value = '%02x%02x%02x' % bgr # convert bgr in hex
+    int_value = int(str_value, 16) # convert hex to integer
+    return int_value
